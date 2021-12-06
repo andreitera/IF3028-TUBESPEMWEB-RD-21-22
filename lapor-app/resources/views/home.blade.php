b63b64637a48e079aba5ee05b1878251314f63fb
Toolop <toolop2312@gmail.com>
2021-12-06 22:50:06 +0700

    menambahkan tampilan home

--- lapor-app/resources/views/home.blade.php
+++ lapor-app/resources/views/home.blade.php
@@ -1,12 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="../css/style_tampilan.css">
+
 </head>
 <body>
-    halaman home
+	<div class="container">
+		<div class="content">
+		<div class="header">
+			<strong>SIMPLE LAPOR!</strong>	
+			<br><br><br>
+		</div>
+		<form action="" method="POST">	
+			<center>
+				<input type="text" name="cari">
+				<button type="submit" name="btncari" style="width: 10%; ">Cari</button>
+			</center>
+		</form>
+		<br>
+		<center>
+			<a href="">Buat Laporan/Komentar 
+			<div style="margin-top: -18px; margin-left: 195px;">
+				<a>
+
+					<img src="tambah.jpg" width="13px" height="13px">
+
+				</a>	
+			
+			</div>
+
+		</center>
+		
+
+		<br>
+		
+			<p>Laporan/Komentar Terakhir</p>
+		
+		<hr>
+
+		
+			 
+			
+			 <p><img src="file.png" width="20px" height="20px"></p>
+
+			 <div style="margin-top: -40px; margin-bottom: 17px; margin-left: 25px; width: 290px;">
+
+			 Lampiran: 
+
+			 </div>
+			 
+			 <p style='width: auto; height: auto; margin-top:-35.9px; margin-left: 325px;'>Waktu:</p>
+		
+		
+		
+		<p style="margin-top: -33.7px; margin-left: 535px;">
+			<a href="">Lihat Selengkapnya
+		</p>
+
+		<div style="margin-top: -34px; margin-left: 678px;">
+	
+					<img src="next-button.png" width="20px" height="20px">
+				</a>	
+		</div>
+		<hr>
+		
+		</div>
+
+		<br><br>
+	</div>
+
 </body>
 </html>

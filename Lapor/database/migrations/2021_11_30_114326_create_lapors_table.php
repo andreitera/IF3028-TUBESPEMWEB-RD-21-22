a3c9f8e57ead508b4039dbd381a9a6a04826b240
sophianrs <72431579+sophianrs@users.noreply.github.com>
2021-12-06 10:49:37 +0700

    Add files via upload

---
+++ Lapor/database/migrations/2021_11_30_114326_create_lapors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaporsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lapors', function (Blueprint $table) {
+            $table->id();
+            $table->text('isi');
+            $table->string('aspek');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lapors');
+    }
+}

eb3e53b1283aeb060bce4a7a5a3dff9b49d7e0e8
HRFI-B <ihza.119140130@student.itera.ac.id>
2021-12-09 23:18:03 +0700

    Pembuatan halaman Homepage

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Home.css">
+    <title>Lapor</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="title">
+                <h1>SIMPLE LAPOR!</h1>
+            </div>
+            <div class="search_bar">
+                <div class="search_btn">
+                    Cari
+                </div>
+            </div>
+            <div class="buat_lapor">
+                <a href="">Buat Laporan/Komentar</a>
+            </div>
+        </div>
+        <div class="container-laporan">
+            <!-- Contoh sebelum menggunakan data dari DB -->
+            <div class="header-laporan">
+                Laporan/Komentar Terakhir 
+                <br>
+                <hr> 
+            </div>
+            <div class="cuplikan-laporan">
+                <div class="paragraph">
+                    <p>
+                        I inadvertently went to See's Candy last week (I was in the mall looking for phone repair), and as it turns out, See's Candy now charges a dollar -- a full dollar -- for even the simplest of their wee confection offerings. I bought two chocolate lollipops and two chocolate-caramel-almond things. The total cost was four-something. I mean, the candies were tasty and all, but let's be real: A Snickers bar is fifty cents. After this dollar-per-candy revelation, I may not find myself wandering dreamily back into a See's Candy any time soon.
+                    </p>
+                </div>
+                <div class="lampiran">
+                    <img id="gambar-lampiran" src="./image/test-image-1.jpg">
+                </div>
+                <div class="footer-laporan">
+                    <div class="nama-gambar">
+                        Lampiran: test-image-1.jpg
+                    </div>
+                    <div class="waktu-laporan">
+                        Waktu: 01-02-2021
+                    </div>
+                    <div class="lihat-selengkapnya">
+                        <a href="">Lihat Selengkapnya &rarr; </a>
+                    </div>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

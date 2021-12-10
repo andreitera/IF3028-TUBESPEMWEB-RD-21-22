e1f4fd6b2a37f93be080c2087f708a6518893fcd
GilangAsh14 <91508048+GilangAsh14@users.noreply.github.com>
2021-12-10 20:38:08 +0700

    Menambah fungsi insert data di model

--- tubes/application/models/M_lapor.php
+++ tubes/application/models/M_lapor.php
@@ -5,8 +5,12 @@ class M_lapor extends CI_Model {
 	public function getLapor(){
 		$this->db->select('*');
 		$this->db->from('lapor');
+		$this->db->order_by('id', 'desc');
 		$query = $this->db->get();
 		return $query->result();
+	}
+	public function insert_data($data){
+		$this->db->insert('lapor', $data);
 	}
 }
 ?>

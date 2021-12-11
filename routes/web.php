7f0f79f37f7c80cc2475d1e5f1686aa57b13e69c
HRFI-B <ihza.119140130@student.itera.ac.id>
2021-12-11 13:38:34 +0700

    Update Routing (Penjelasan di deskripsi)
    -Meredirect "/" ke "/Home"
    -Menambahkan route ke "/buat" yang menuju ke "Buat.blade.php"

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect('/home');
+});
 
 Route::get('/home', function () {
     return view('Home');
+});
+
+Route::get('/buat', function () {
+    return view('Buat');
 });

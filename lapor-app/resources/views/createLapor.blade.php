1a71ace133530e48e28f10ee3e7d29077e3b6a8c
nizariapipit <nizariap@gmail.com>
2021-12-18 00:57:40 +0700

    mencoba

--- lapor-app/resources/views/createLapor.blade.php
+++ lapor-app/resources/views/createLapor.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert Laporan</title>
-    <link rel="stylesheet" href="../css/style_insert.css">
+    <link href="{{ asset('../css/style_insert.css') }}" rel="stylesheet" type="text/css" >
 </head>
 <body>
     <div class="container">	

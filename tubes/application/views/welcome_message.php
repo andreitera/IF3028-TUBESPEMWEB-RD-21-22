ff2d141d0508c0de258904c428cb50f467d846e0
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2021-12-18 11:31:30 +0700

    Fix layout

--- tubes/application/views/welcome_message.php
+++ tubes/application/views/welcome_message.php
@@ -22,12 +22,13 @@
 				<hr>
 				<?php
 					foreach ($data_lapor as $d) {
+						echo "<br><div class='isi-lapor'>";
 						echo $d->isi;
-						
+						echo "</div>";
 				?>
 				<div><br>Lampiran: <?php echo $d->file; ?>
 					<div class="opsional">Waktu: <?php echo $d->waktu; ?> 
-					<a  href="<?php echo base_url('Welcome/detail_laporan')?>"> &emsp;Lihat Selengkapnya></a>
+					<a  href="<?php echo base_url('Welcome/detail_laporan/'.$d->id) ?>"> &emsp;Lihat Selengkapnya></a>
 					</div>
 					<br>
 					<hr>

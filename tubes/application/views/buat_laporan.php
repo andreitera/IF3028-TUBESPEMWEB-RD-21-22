fe0aeca8e4df8a3d68243e490687e3b5b9d0454d
GilangAsh14 <91508048+GilangAsh14@users.noreply.github.com>
2021-12-22 10:33:46 +0700

    Update buat_laporan.php

--- tubes/application/views/buat_laporan.php
+++ tubes/application/views/buat_laporan.php
@@ -3,18 +3,21 @@
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+	<script src="<?php echo base_url(); ?>js/script.js"></script>
+	
 </head>
 <body>
 	<div class="container">
 		<fieldset>	
 			<div class="content">
 				<div class="header">
-					<h2>Layanan Aspirasi dan Pengaduan Online Mahasiswa ITERA</h2><br>	
+					<a href="<?php echo base_url()?>"><h1>SIMPLE LAPOR!</h1><br></a>
 				</div>
 				<p>Detail Laporan/Komentar	</p>
 				<hr>
-				<form action="<?php echo base_url('Welcome/aksi_insert')?>" method="post">
-					<textarea class="boxlapor" type="text" name="isi" placeholder="Laporan/Komentar" require></textarea><br><br>	
+				
+				<?php echo form_open_multipart('Welcome/tambah_aksi'); ?>
+					<textarea id = "boxlapor" class="boxlapor" type="text" name="isi" placeholder="Laporan/Komentar" require></textarea><br><br>	
 					<select id="aspek" name="aspek" class="boxaspek" require>
 						<option disabled selected>Pilih Aspek Pelaporan/Komentar</option>
 						<option value="infrastruktur">Infrastrukur</option>
@@ -23,10 +26,17 @@
 						<option value="mahasiswa">Mahasiswa</option>
 						<option value="pengajaran">Pengajaran</option>
 					</select><br><br>	
-					<input type="file" name="file" class="boxupload"><br><br><br>	
-					<input class="buttonsubmit" type="submit" name="submit" value="Buat LAPOR!">
-				</form>
-			</div>
+					<input type="file" name="file" class="boxupload" id="file" >
+					<br><br><br>	
+					<input onclick ="return cek_kata()" class="buttonsubmit" type="submit" name="submit" value="Buat LAPOR!">
+				<?php echo form_close(); ?>
+				<br><br><br>
+				<hr>
+				<div class="footer">
+					<p>&copy Copyright by Frinaldo Sinaga - 119140064 | Gilang Ashari Abimanyu - 119140040</p>
+					<span>2021</span>
+				</div>
+			</div> 
 		</fieldset>
 	</div>
 

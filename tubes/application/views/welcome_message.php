82334252a4edd2957fa26d8076a66e34c1ac3451
Frinaldo Sinaga <84637791+Sinagafrinaldo@users.noreply.github.com>
2021-12-22 10:38:38 +0700

    Fix pagination halaman, dan search

--- tubes/application/views/welcome_message.php
+++ tubes/application/views/welcome_message.php
@@ -9,35 +9,58 @@
 		<fieldset>	
 			<div class="content">
 				<div class="header">
-					<h2>SIMPLE LAPOR!</h2><br>	
+					<a href="<?php echo base_url()?>"><h1>SIMPLE LAPOR!</h1><br></a>
 				</div>
-				<form action="form.php" method="post"> 
-					<input class="boxcari" type="text" name="term" />
-					<input class="btncari"type="submit" value="Cari" /> 
+				<form action="<?php echo base_url('Welcome/cari') ?>" method="get"> 
+					<input class="boxcari" type="text" name="keyword" />
+					<button class="btncari" type="submit"><img class="ikon-cari" src="<?php echo base_url(); ?>assets/ikon-cari.png">&nbsp;Cari</button>
 				</form> <br>
+
+	
 				<p align="center">
-					<a href="<?php echo base_url('Welcome/buat_laporan') ?>" class="buat-lapor">Buat Laporan/Komentar +</a>
+					<a href="<?php echo base_url('Welcome/buat_laporan') ?>" class="buat-lapor">Buat Laporan/Komentar <b>+</b></a>
 				</p>
+
+
 				<p>Laporan/Komentar Terakhir</p>
 				<hr>
 				<?php
-					foreach ($data_lapor as $d) {
+				
+				$no = $this->uri->segment('3') + 1;
+					foreach ($lapor as $d) {
 						echo "<br><div class='isi-lapor'>";
 						echo $d->isi;
 						echo "</div>";
 				?>
-				<div><br>Lampiran: <?php echo $d->file; ?>
+				<br>
+				
 					<div class="opsional">Waktu: <?php echo $d->waktu; ?> 
-					<a  href="<?php echo base_url('Welcome/detail_laporan/'.$d->id) ?>"> &emsp;Lihat Selengkapnya></a>
+						<a  href="<?php echo base_url('Welcome/detail_laporan/'.$d->id) ?>"> &emsp;Lihat Selengkapnya></a>
 					</div>
-					<br>
-					<hr>
-				</div>
-			
+					<div>Lampiran:  <span class="judul-lampiran"> </span><?php echo $d->file; ?></div>
+				<hr>
 				
 				<?php
 					}
 				?>
+				<br>
+				<br>
+				<div class="halaman">
+					<?php 
+					echo $this->pagination->create_links();
+					?>
+				</div>
+				
+				<div class="titik">
+					<p>.</p>
+					<p>.</p>
+					<p>.</p>
+				</div>
+				<hr>
+				<div class="footer">
+					<p>&copy Copyright by Frinaldo Sinaga - 119140064 | Gilang Ashari Abimanyu - 119140040</p>
+					<span>2021</span>
+				</div>
 			</div>
 		</fieldset>
 	</div>

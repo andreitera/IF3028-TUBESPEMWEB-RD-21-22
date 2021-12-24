d703b3c4f4b0c2bf7fd5c0690f18168accdf091d
agustohr <agusto.hawlai@gmail.com>
2021-12-24 16:40:20 +0700

    base frontend

---
+++ Frontend/buatLaporan.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tubes</title>
+    <link rel="stylesheet" type= "text/css" href="styleBuatLaporan.css">
+</head>
+<body>
+    <div class="container">
+        <div class="atas">
+            <nav>
+                <div class="logo"><img src="file_gambar/Logo_ITERA.png" alt="logo itera" height="85px"></div>
+                <ul>
+                    <li><a href="#"><b>TENTANG LAPOR</b></a></li>
+                    <li><a href="#"><b>STATISTIK</b></a></li>  
+                    <li><a href="#"><b>LAPORAN</a></b></li>
+                </ul>
+            </nav>
+
+            <header>
+                
+                <div class="judul">
+                    <h2>Layanan Aspirasi dan Pengaduan Online Mahasiswa Teknik Informatika ITERA</h2><br>
+                    <p>Sampaikan laporan Anda Langsung Kepada Prodi Teknik Informatika</p><br>
+                    <hr>
+                </div>
+                
+            </header>
+        </div>
+        
+
+        <div class="body">  
+            <h1>Sampaikan Laporan Anda</h1>
+            
+            <p>Buat Laporan/Komentar</p>
+            <br><hr><br>
+            <form action="">
+                <textarea name="laporan" id="" cols="176" rows="20" placeholder="Laporan/Komentar"></textarea>
+                <br><br>
+                <select class="aspekLapor" name="aspek" id="">
+                    <option value="">Pilih Aspek Pelaporan/Komentar</option>
+                </select>
+                <br><br>
+                <input type="date">
+                <br><br>
+                <input type="file">
+                <br>
+                <button class="submitLapor" type="submit">Buat Lapor</button>
+                <br><br>
+                
+                
+                <br>
+            </form>
+
+
+
+
+            
+        </div>
+            
+
+    </div>
+
+</body>
+</html>
---
+++ Frontend/utama.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tubes</title>
+    <link rel="stylesheet" type= "text/css" href="styleutama.css">
+</head>
+<body>
+    <div class="container">
+        <div class="atas">
+            <nav>
+                <div class="logo"><img src="file_gambar/Logo_ITERA.png" alt="logo itera" height="85px"></div>
+                <ul>
+                    <li><a href="#"><b>TENTANG LAPOR</b></a></li>
+                    <li><a href="#"><b>STATISTIK</b></a></li>  
+                    <li><a href="#"><b>LAPORAN</a></b></li>
+                </ul>
+            </nav>
+
+            <header>
+                
+                <div class="judul">
+                    <h2>Layanan Aspirasi dan Pengaduan Online Mahasiswa Teknik Informatika ITERA</h2><br>
+                    <p>Sampaikan laporan Anda Langsung Kepada Prodi Teknik Informatika</p><br>
+                    <hr>
+                </div>
+                
+            </header>
+        </div>
+        
+
+        <div class="body">
+            
+            <div class="search">
+                <form action="" method="post">
+                    <input type="text" name="cari" placeholder="Search">
+                    <button type="submit" name="cari">Cari</button>
+                </form>
+            </div>
+            
+            <div class="buat">
+                <table>
+                    <tr>
+                        <td><p>Buat Laporan/Komentar</p></td>
+                        <td><a href="buatLaporan.php"><img src="file_gambar/add_box_black_24dp.png" alt=""></a></td>
+                    </tr>
+                </table>
+                
+                
+            </div>
+
+            
+                <div class="laporan">
+                    <p>Laporan/Komentar Terakhir</p>
+                    
+                    <br><hr><br><br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae itaque, tenetur nesciunt debitis eum nemo totam nihil nostrum iure. Nostrum reprehenderit, fugiat et iure cum dignissimos! Neque mollitia sit nemo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ut, blanditiis molestiae mollitia similique et eos dolor assumenda dolore sint quae voluptates labore, esse vitae facilis dolores porro? Sed, ea?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi maiores natus nostrum quod harum quisquam fugiat, rerum reiciendis nobis blanditiis aspernatur accusantium, consequatur odit quibusdam sunt exercitationem, laborum modi.</p>
+                    <br>
+                    <div class="lampiran">
+                        <p>Lampiran : Gambar.jpg</p>
+                    </div>
+                    <div class="waktu">
+                        <table>
+                            <tr>
+                                <td><p>Waktu : 21-12-2021 15:00</p></td>
+                                <td><a href="detail.php">Lihat Selengkapnya</a></td>
+                                <td><img src="file_gambar/chevron_right_black_24dp.png" alt=""></td>
+                            </tr>
+                        </table>
+                    </div><br>
+
+                    <br><hr><br><br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae itaque, tenetur nesciunt debitis eum nemo totam nihil nostrum iure. Nostrum reprehenderit, fugiat et iure cum dignissimos! Neque mollitia sit nemo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ut, blanditiis molestiae mollitia similique et eos dolor assumenda dolore sint quae voluptates labore, esse vitae facilis dolores porro? Sed, ea?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi maiores natus nostrum quod harum quisquam fugiat, rerum reiciendis nobis blanditiis aspernatur accusantium, consequatur odit quibusdam sunt exercitationem, laborum modi.</p>
+                    <br>
+                    <div class="lampiran">
+                        <p>Lampiran : Gambar.jpg</p>
+                    </div>
+                    <div class="waktu">
+                        <table>
+                            <tr>
+                                <td><p>Waktu : 21-12-2021 15:00</p></td>
+                                <td><a href="detail.php">Lihat Selengkapnya</a></td>
+                                <td><img src="file_gambar/chevron_right_black_24dp.png" alt=""></td>
+                            </tr>
+                        </table>
+                    </div><br>
+
+                    <br><hr><br><br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae itaque, tenetur nesciunt debitis eum nemo totam nihil nostrum iure. Nostrum reprehenderit, fugiat et iure cum dignissimos! Neque mollitia sit nemo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ut, blanditiis molestiae mollitia similique et eos dolor assumenda dolore sint quae voluptates labore, esse vitae facilis dolores porro? Sed, ea?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi maiores natus nostrum quod harum quisquam fugiat, rerum reiciendis nobis blanditiis aspernatur accusantium, consequatur odit quibusdam sunt exercitationem, laborum modi.</p>
+                    <br>
+                    <div class="lampiran">
+                        <p>Lampiran : Gambar.jpg</p>
+                    </div>
+                    <div class="waktu">
+                        <table>
+                            <tr>
+                                <td><p>Waktu : 21-12-2021 15:00</p></td>
+                                <td><a href="detail.php">Lihat Selengkapnya</a></td>
+                                <td><img src="file_gambar/chevron_right_black_24dp.png" alt=""></td>
+                            </tr>
+                        </table>
+                    </div><br>
+
+                    <br><hr><br><br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae itaque, tenetur nesciunt debitis eum nemo totam nihil nostrum iure. Nostrum reprehenderit, fugiat et iure cum dignissimos! Neque mollitia sit nemo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ut, blanditiis molestiae mollitia similique et eos dolor assumenda dolore sint quae voluptates labore, esse vitae facilis dolores porro? Sed, ea?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa animi maiores natus nostrum quod harum quisquam fugiat, rerum reiciendis nobis blanditiis aspernatur accusantium, consequatur odit quibusdam sunt exercitationem, laborum modi.</p>
+                    <br>
+                    <div class="lampiran">
+                        <p>Lampiran : Gambar.jpg</p>
+                    </div>
+                    <div class="waktu">
+                        <table>
+                            <tr>
+                                <td><p>Waktu : 21-12-2021 15:00</p></td>
+                                <td><a href="detail.php">Lihat Selengkapnya</a></td>
+                                <td><img src="file_gambar/chevron_right_black_24dp.png" alt=""></td>
+                            </tr>
+                        </table>
+                    </div><br>
+                     
+                </div>  
+                                    
+            
+        </div>
+            
+
+    </div>
+
+</body>
+</html>

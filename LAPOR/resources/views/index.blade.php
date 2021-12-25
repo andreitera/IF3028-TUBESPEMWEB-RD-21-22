2af4ae700690534987f1ba1f2b6d38ec6db9cdd1
Muhammad119140127 <muhammad.119140127@student.itera.ac.id>
2021-12-26 00:17:15 +0700

    fix index blade

--- LAPOR/resources/views/index.blade.php
+++ LAPOR/resources/views/index.blade.php
@@ -10,9 +10,9 @@
 <body>
   <h1>SIMPLE LAPOR !</h1>
   <div class="container center">
-    <form action="#" method="GET">
+    <form action="{{ route('index') }}" method="GET">
       <div class="search">
-        <input name="keyword" type="text" class="search" placeholder="Cari disini . . .">
+        <input name="keyword" type="text" class="searchBar" placeholder="Cari disini . . .">
         <button type="submit" class="searchbtn">
           <i class="fa fa-search"></i>
           Cari
@@ -21,7 +21,7 @@
       </div>
     </form>
 
-    <a href="#" class="center">Buat laporan/Komentar <i class="fa fa-plus-square"></i></a>
+    <a href="{{ route('create') }}" class="center">Buat laporan/Komentar <i class="fa fa-plus-square"></i></a>
   </div>
 
   <div class="container">
@@ -29,24 +29,31 @@
       <div class="col text-left">Laporan/Komentar Terakhir</div>
     </div>
 
+    @foreach ($posts as $post)
     <div class="content">
       <div class="content-body">
-        Lorem ipsum sit dolor amet
+        {!! nl2br(e($post->isilaporan)) !!}
       </div>
       <div class="row content-footer">
 
-        <a href="#">
+        @if($post->lampiran)
+        <a href="{{ $post->lampiran }}">
           <div class="col text-left">Unduh Lampiran</div>
         </a>
+        @endif
         <div class="col text-right">
-          <span>Waktu : 23-12-2021</span>
-          <span><a href="#">Lihat Selengkapnya <i class="fa fa-angle-right"></i></a></span>
+          <span>Waktu : {{ $post->created_at }}</span>
+          <span><a href="{{ route('show', $post->id) }}">Lihat Selengkapnya <i class="fa fa-angle-right"></i></a></span>
         </div>
       </div>
     </div>
-
+    @endforeach
   </div>
-
+  @if(session('status'))
+  <script>
+    alert("{{ session('status') }}");
+  </script>
+  @endif
 </body>
 
 

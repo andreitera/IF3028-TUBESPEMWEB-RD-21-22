3ee810532a68d43cc7fe09a56b71eb127b41dc6f
HRFI-B <84057899+Ihza-Fajrur@users.noreply.github.com>
2021-12-25 22:52:42 +0700

    Add section

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,22 +1,24 @@
+@extends ('layout/main')
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        @section ('head')
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SIMPLE LAPOR!</title>
         <link rel="stylesheet" type="text/css" href="style_detail.css">
+        @endsection
     </head>
     <body>
+        @section ('container')
         <div class="container">
             <h1>SIMPLE LAPOR!</h1>
             <br><br>
             <h4>Detail Laporan/Komentar</h4>
             <hr>
             <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aliquid expedita velit tenetur assumenda, eum quaerat esse ex quod veritatis sit sint voluptatibus architecto eius aspernatur repudiandae, quis nesciunt ratione.</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, pariatur vitae distinctio amet iusto soluta, asperiores deleniti reiciendis facilis fugiat rem. Eum qui, numquam dolore dolorem exercitationem possimus facere reiciendis?</p>
+            {{-- Add Paragraph here --}}
             <br>
             <h4>Lampiran :</h4>
             <br>
@@ -26,5 +28,6 @@
             <p>Infrastruktur:</p>
             <a href="" ><h4>Hapus Laporan/Komentar</h4></a>
         </div>
+        @endsection
     </body>
 </html>

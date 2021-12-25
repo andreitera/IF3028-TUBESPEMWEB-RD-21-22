676a745fcddcf66d5a27b5d43d9770cfc4147655
Muhammad119140127 <muhammad.119140127@student.itera.ac.id>
2021-12-25 23:53:49 +0700

    fix model

--- LAPOR/app/Models/Laporan.php
+++ LAPOR/app/Models/Laporan.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Laporan extends Model
 {
     use HasFactory;
+    protected $table = 'laporan';
+    protected $fillable = ['isilaporan', 'aspek', 'lampiran'];
 }
